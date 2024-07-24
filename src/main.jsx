457f91999aacd4a6563049c3febe3e498de5e22a
Yagnesh Modh <contact@thespecialcharacter.com>
2024-07-24 11:00:37 +0530

    commit code

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import ReactDOM from "react-dom/client";
-import Todo from "./TodoApp/todo.jsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Todo />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
---
+++ src/router.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router-dom";
+import Todo from "./TodoApp/todo.jsx";
+import Login from "./screens/login.jsx";
+import Register from "./screens/register.jsx";
+import AuthLayout from "./layouts/authLayout.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Todo />,
+  },
+  {
+    path: "/contact",
+    element: <h1>hello world</h1>,
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        index: true,
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+export default router;
