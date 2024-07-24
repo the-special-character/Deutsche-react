a3d181c7ffc726261feaad7f84165694ddc65335
Yagnesh Modh <contact@thespecialcharacter.com>
2024-07-24 11:47:49 +0530

    commit code

--- src/screens/register.jsx
+++ src/screens/register.jsx
@@ -1,94 +1,162 @@
-import { PureComponent } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
-export default class Register extends PureComponent {
-  render() {
-    return (
-      <form action="#" method="POST" className="space-y-6">
-        <div>
-          <label
-            htmlFor="name"
-            className="block text-sm font-medium leading-6 text-gray-900"
-          >
-            Name
-          </label>
-          <div className="mt-2">
-            <input
-              id="name"
-              name="name"
-              type="text"
-              required
-              autoComplete="name"
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
+const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    mode: "all",
+  });
+  const navigate = useNavigate();
 
-        <div>
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium leading-6 text-gray-900"
-          >
-            Email address
-          </label>
-          <div className="mt-2">
-            <input
-              id="email"
-              name="email"
-              type="email"
-              required
-              autoComplete="email"
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
+  const onSubmit = async (data) => {
+    try {
+      const res = await fetch("http://localhost:3000/register", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      const json = await res.json();
+      console.log(json);
+      navigate("/");
+    } catch (error) {}
+  };
 
-        <div>
-          <label
-            htmlFor="password"
-            className="block text-sm font-medium leading-6 text-gray-900"
-          >
-            Password
-          </label>
-          <div className="mt-2">
-            <input
-              id="password"
-              name="password"
-              type="password"
-              required
-              autoComplete="new-password"
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+      <div>
+        <label
+          htmlFor="name"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Name
+        </label>
+        <div className="mt-2">
+          <input
+            id="name"
+            type="text"
+            autoComplete="name"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            {...register("name", {
+              required: {
+                value: true,
+                message: "Name is required..",
+              },
+            })}
+          />
         </div>
+        {errors?.name && (
+          <p className="text-red-400 text-sm">{errors?.name?.message}</p>
+        )}
+      </div>
 
-        <div>
-          <label
-            htmlFor="confirm-password"
-            className="block text-sm font-medium leading-6 text-gray-900"
-          >
-            Confirm Password
-          </label>
-          <div className="mt-2">
-            <input
-              id="confirm-password"
-              name="confirm-password"
-              type="password"
-              required
-              autoComplete="new-password"
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
+      <div>
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Email address
+        </label>
+        <div className="mt-2">
+          <input
+            id="email"
+            type="email"
+            autoComplete="email"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            {...register("email", {
+              required: {
+                value: true,
+                message: "Email is required..",
+              },
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "Enter valid email address",
+              },
+            })}
+          />
         </div>
+        {errors?.email && (
+          <p className="text-red-400 text-sm">{errors?.email?.message}</p>
+        )}
+      </div>
 
-        <div>
-          <button
-            type="submit"
-            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Sign in
-          </button>
+      <div>
+        <label
+          htmlFor="password"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Password
+        </label>
+        <div className="mt-2">
+          <input
+            id="password"
+            type="password"
+            autoComplete="new-password"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            {...register("password", {
+              required: {
+                value: true,
+                message: "Password is required..",
+              },
+              pattern: {
+                value:
+                  /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/,
+                message: "Enter valid password",
+              },
+            })}
+          />
         </div>
-      </form>
-    );
-  }
-}
+        {errors?.password && (
+          <p className="text-red-400 text-sm">{errors?.password?.message}</p>
+        )}
+      </div>
+
+      <div>
+        <label
+          htmlFor="confirm-password"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Confirm Password
+        </label>
+        <div className="mt-2">
+          <input
+            id="confirm-password"
+            type="password"
+            autoComplete="new-password"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            {...register("confirmPassword", {
+              required: {
+                value: true,
+                message: "Confirm Password is required..",
+              },
+              validate: (value) =>
+                value === watch("password") || "Passwords do not match",
+            })}
+          />
+        </div>
+        {errors?.confirmPassword && (
+          <p className="text-red-400 text-sm">
+            {errors?.confirmPassword?.message}
+          </p>
+        )}
+      </div>
+
+      <div>
+        <button
+          type="submit"
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Sign in
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Register;
